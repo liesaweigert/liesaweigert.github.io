1d8fbd753b1827b501d80aef04be94e73362ab0f
liesaweigert <liesa.weigert@mac.com>
2015-05-31 15:06:08 +0200

    Updated file system

---
+++ javascript/liesa.js
@@ -0,0 +1,25 @@
+var main = function() {
+
+    //parallax scrolling with stellar
+    $.stellar({
+          // Set scrolling to be in either one or both directions
+          horizontalScrolling: false,
+          verticalScrolling: true,
+
+          // Refreshes parallax content on window load and resize
+          responsive: true,
+
+          // Select which property is used to calculate scroll.
+          // Choose 'scroll', 'position', 'margin' or 'transform',
+          // or write your own 'scrollProperty' plugin.
+          scrollProperty: 'scroll',
+
+          // Select which property is used to position elements.
+          // Choose between 'position' or 'transform',
+          // or write your own 'positionProperty' plugin.
+          positionProperty: 'position',
+    });
+
+};
+
+$(document).ready(main);
