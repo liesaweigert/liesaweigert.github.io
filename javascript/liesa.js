1529449476bb3ca21ace9dbd3317b3d5e4533141
liesaweigert <liesa.weigert@mac.com>
2015-08-25 18:17:19 +0200

    changed stellar.js to parallax.js in hopes of better performance

--- javascript/liesa.js
+++ javascript/liesa.js
@@ -1,25 +1,4 @@
 var main = function() {
-
-    //parallax scrolling with stellar
-    $.stellar({
-          // Set scrolling to be in either one or both directions
-          horizontalScrolling: false,
-          verticalScrolling: true,
-
-          // Refreshes parallax content on window load and resize
-          responsive: true,
-
-          // Select which property is used to calculate scroll.
-          // Choose 'scroll', 'position', 'margin' or 'transform',
-          // or write your own 'scrollProperty' plugin.
-          scrollProperty: 'scroll',
-
-          // Select which property is used to position elements.
-          // Choose between 'position' or 'transform',
-          // or write your own 'positionProperty' plugin.
-          positionProperty: 'position',
-    });
-
     //sticky navbar
     $(".navbar").sticky({topSpacing:0});
 
